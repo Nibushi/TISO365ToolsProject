4cccfbe775966baa5174630a1bed01ee7d11c8de
Nibushi <si.alexander@gmail.com>
2017-07-15 20:07:52 +0100

    Initial Commit of Code

---
+++ Tests/TISO365ToolsModuleSetup.tests.ps1
@@ -0,0 +1,73 @@
+$projectRoot = Resolve-Path "$PSScriptRoot\.."
+$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psd1")
+$moduleName = Split-Path $moduleRoot -Leaf
+
+Describe "$moduleName Module Test" {
+    Context "Verifying $moduleName setup"{
+        #Check for Module
+        it "has the root module $moduleName.psm1" {
+            "$moduleRoot\$moduleName.psm1" | Should Exist 
+        }
+
+        #Check for manifest file associated with module 
+            It "has a manifest file called $moduleName.psd1 associated with $moduleName" { 
+                "$moduleRoot\$moduleName.psd1" | Should Exist 
+                "$moduleRoot\$moduleName.psd1" | Should Contain $moduleName
+        }
+    }
+
+
+    $moduleFunctionScripts = Get-ChildItem $moduleRoot -Include *.ps1 -Exclude *.tests.ps1  -Recurse
+
+    # TestCases are splatted to the script so we need hashtables
+    # $testCase = $moduleFunctionScripts | Foreach-Object {@{file = $_;TestName = $_.Name}}         
+
+    # It "Script <TestName> should be valid powershell" -TestCases $testCase {
+    #     param($file)
+
+    #     $file.fullname | Should Exist
+    #     $file.fullname | Should Contain "function $f"
+    # }
+
+    foreach($mfs in $moduleFunctionScripts ){
+        $fileName = Split-Path $mfs.fullname -leaf
+        $funcName = $fileName.Split('.')[0]
+
+        Context "Verifying $fileName set up"{
+            It "has a $fileName file"{
+                $mfs.fullname | Should Exist
+            }
+
+            It "$fileName should have a function called $funcName"{
+                $mfs.fullname | Should ContainExactly "function $funcName\b"
+            }
+
+            It "$fileName should have a help block" { 
+                $mfs.fullname | Should Contain '<#' 
+                $mfs.fullname | Should Contain '#>' 
+            } 
+
+            It "$fileName should have a SYNOPSIS section in the help block" { 
+                $mfs.fullname | Should Contain '.SYNOPSIS'                 
+            } 
+
+            It "$fileName should have a DESCRIPTION section in the help block" { 
+                $mfs.fullname | Should Contain '.DESCRIPTION'                 
+            }
+
+            It "$fileName should have a EXAMPLE section in the help block" { 
+                $mfs.fullname | Should Contain '.EXAMPLE'                 
+            }
+
+            It "$fileName should be an advanced function" { 
+                $mfs.fullname | Should Contain 'function'                 
+                $mfs.fullname | Should Contain 'cmdletbinding()' 
+                $mfs.fullname | Should Contain 'param'                                 
+            } 
+
+            It "$fileName should contain Write-Verbose blocks" { 
+                $mfs.fullname | Should Contain 'Write-Verbose'                 
+            } 
+        } 
+    }
+}
---
+++ default.ps1
@@ -0,0 +1,28 @@
+Task Test -description "Run Pester Tests" {
+    PowerShell.exe -NoProfile -NonInteractive -Command "Invoke-Pester -EnableExit -quiet"
+}
+
+Task UpdateModuleManifest -description "Add Public Functions to module manifest" -depends Test -precondition {$LASTEXITCODE -eq 0}{
+    $projectRoot = Resolve-Path "$PSScriptRoot"
+    $manifestPath = Resolve-Path "$projectRoot\*\*.psd1"
+    $moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psd1")
+
+    if(Test-Path $moduleRoot\Public){
+        $publicFunctions = Get-ChildItem $moduleRoot\Public
+
+        $funcsToExport = foreach($pf in $publicFunctions){
+            $fileName = Split-Path $pf.fullname -leaf
+            $funcName = $fileName.Split('.')[0]
+            $funcName
+        }
+
+        $params = @{
+            Path = $manifestPath
+            FunctionsToExport = $funcsToExport
+        }
+
+        Update-ModuleManifest @params
+    }
+}
+
+Task Default -depends UpdateModuleManifest
