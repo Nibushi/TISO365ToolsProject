00710e49eb3f88680965116b2da8ab1081790892
Nibushi <si.alexander@gmail.com>
2017-07-16 14:22:42 +0100

    Add Bulk User Function

---
+++ TISO365Tools/Public/Add-TISBulkUsers.ps1
@@ -0,0 +1,22 @@
+function Add-TISBulkUsers{
+ <#
+ .SYNOPSIS
+ Short description
+ 
+ .DESCRIPTION
+ Long description
+ 
+ .EXAMPLE
+ An example
+ 
+ .NOTES
+ General notes
+ #>
+ [CmdletBinding()]
+ Param(
+
+ )
+
+ Write-Verbose "Test"
+    
+}
